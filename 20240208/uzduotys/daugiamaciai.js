d8121a2c9eaf15f0e21bd876519c959cd4dc3d15
LordsVoldemorts <petrauskiene.ieva1993@gmail.com>
2024-02-13 15:46:31 +0200

    buttons, mygtukai, object

--- 20240208/uzduotys/daugiamaciai.js
+++ 20240208/uzduotys/daugiamaciai.js
@@ -8,10 +8,10 @@ function rand(min, max) {
 
 const array = [];
 
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     array[i] = [];
 
-    for(let x = 0; x < 5; x++) {
+    for (let x = 0; x < 5; x++) {
         array[i][x] = rand(5, 25)
     }
 }
@@ -22,10 +22,10 @@ console.log(array);
 
 let sum = 0;
 
-for(let i = 0; i < 10; i++) {
-    for(let x = 0; x < 5; x++) {
+for (let i = 0; i < 10; i++) {
+    for (let x = 0; x < 5; x++) {
         if (array[i][x] > 10)
-        sum++
+            sum++
     }
 }
 
@@ -36,107 +36,161 @@ console.log(sum)
 
 let max = 0;
 
-for(let i = 0; i < 10; i++) {
-    for(let x = 0; x < 5; x++) {
-        if(array [x][i] > max)
-        max = array[x][i]
+for (let i = 0; i < 10; i++) {
+    for (let x = 0; x < 5; x++) {
+        if (array[x][i] > max)
+            max = array[x][i]
     }
 }
-console.log (max)
+console.log(max)
 
 // 2c uzd. Suskaičiuokite kiekvieno antro lygio masyvų su vienodais indeksais sumas (t.y. suma reikšmių turinčių indeksą 0, 1 ir t.t.);
 
 let suma = 0;
 let sumos = [0, 0, 0, 0, 0];
 
-for(let i = 0; i < array.length; i++) {
-    
-    for(let x = 0; x < array[i].length; x++) {
-       
+for (let i = 0; i < array.length; i++) {
+
+    for (let x = 0; x < array[i].length; x++) {
+
         sumos[x] += array[i][x]
-        
+
     }
 
 }
 
-console.log ('suma', sumos)
+console.log('suma', sumos)
 
 
 // 2d uzd. Visus antro lygio masyvus “pailginkite” iki 7 elementų;
- 
 
-for (let i = 0; i < array.length; i++){ 
-    for(let x = 0; x < 2; x++) {
-  numbers = rand(0, 1)
-    array[i].push(numbers);
-    
+
+for (let i = 0; i < array.length; i++) {
+    for (let x = 0; x < 2; x++) {
+        numbers = rand(0, 1)
+        array[i].push(numbers);
+
     }
 }
-console.log ( array)
+console.log(array)
 
 // 2e uzd. Suskaičiuokite kiekvieno iš antro lygio masyvų elementų sumą atskirai ir sumas panaudokite kaip reikšmes sukuriant naują masyvą. T.y. pirma naujo masyvo reikšmė turi būti lygi mažesnio masyvo, turinčio indeksą 0 dideliame masyve, visų elementų sumai ;
-let sums = 0;
-let sumz = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-const miau = []
 
-for(let i = 0; i < array.length; i++) {
-    
-    for(let x = 0; x < array[i].length; x++) {
-       
-        sumz[i] += array[i][x]
-        miau.push(sumz)
-        
+const rowsums = []
+for (let i = 0; i < array.length; i++) {
+    let sum = 0
+
+    for (let x = 0; x < array[x].length; x++) {
+        sum += array[i][x];
+
     }
+    rowsums.push(sum)
 
 }
 
-console.log ('suma', sumz)
-console.log(miau)
+console.log(rowsums)
 
 
 // 3 uzd. Sukurkite masyvą iš 10 elementų. Kiekvienas masyvo elementas turi būti masyvas su atsitiktiniu kiekiu nuo 2 iki 20 elementų. Elementų reikšmės atsitiktinai parinktos raidės iš intervalo A-Z. Išrūšiuokite antro lygio masyvus pagal abėcėlę (t.y. tuos kur su raidėm).
 
-// const masyvs = [];
+const raidziumasyvas = [];
+const letters =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (let y = 0; y < 10; y++) {
+    raidziumasyvas[y] = []
+    const ilgis = rand(2, 20)
+    for (let x = 0; x < ilgis; x++) {
+        raidziumasyvas[y][x] = letters[rand(0, letters.length-1)]
+    }
+}
+console.log(...raidziumasyvas)
 
-// for(let x = 0; x < 10; x++) {
-//     masyvs[x] = [];
-//     let randomnr = rand(2, 20)
-//     for (let y = 0; y < randomnr; y++) {
-//         let randindex = Math.floor(Math.random() * 26);
-//         let randlettrs = String.fromCharCode(65 + randindex)
-//         masyvs[x].push(randlettrs)
-//         masyvs[x].sort
-
-//     }
-// }
-// console.log(masyvs)
-let ar3 = [];
-for (let p = 0; p < 10; p++) {
-  const arrLength = rand(2, 20);
-  const arr = new Array(arrLength);
-  for (let a = 0; a < arr.length; a++) {
-    arr[a] = String.fromCharCode(rand(65, 90));
-  }
-  ar3.push(arr.sort());
+for (let y = 0; y < raidziumasyvas.length; y++) {
+    raidziumasyvas[y].sort()
 }
 
-console.log(ar3);
-
-ar3.sort((a, b) => {
-  // Sort by length first
-  if (a.length !== b.length) {
-    return a.length - b.length;
-  }
-  // If lengths are equal, sort by presence of 'K'
-  if (a.includes('K') && !b.includes('K')) {
-    return -1;
-  } else if (!a.includes('K') && b.includes('K')) {
-    return 1;
-  }
-  // If lengths and presence of 'K' are equal, maintain order
-  return 0;
-});
-
-console.log(ar3);
+console.log(raidziumasyvas)
 
 // 4 uzd. Išrūšiuokite trečio uždavinio pirmo lygio masyvą taip, kad elementai kurių masyvai trumpiausi eitų pradžioje. Masyvai kurie turi bent vieną “K” raidę, visada būtų didžiojo masyvo visai pradžioje.
+
+raidziumasyvas.sort((a, b) => a.length - b.length);
+raidziumasyvas.sort((a, b) => {
+    if(a.includes('K'))
+        return -1;
+
+    return 0;
+})
+
+console.log(raidziumasyvas)
+    // console.log(a.length, b.length);
+   
+    // if(a.length > b.length) {
+    //     return 1
+    // }
+    // if(a.length < b.length) {
+    //     return -1
+    // }
+    // abu lygus
+    // return 0;
+
+
+//  
+
+// const temparray = [15, 4, 8, 400, 1500];
+
+// temparray.sort(function(pirma, antra) {
+//     return 1; //teigiama reiksme
+//     return 0; //nieko nekeicia, paliekam
+//     return -1; //neigiama reiksme;
+
+//     // console.log(pirma, antra)
+// })
+
+// 5 uzd. Sukurkite masyvą iš 10 elementų. Masyvo reikšmes užpildykite pagal taisyklę: generuokite skaičių nuo 0 iki 5. Ir sukurkite tokio ilgio masyvą. Jeigu reikšmė yra 0 masyvo nekurkite. Antro lygio masyvo reikšmes užpildykite atsitiktiniais skaičiais nuo 0 iki 10. Ten kur masyvo nekūrėte reikšmę nuo 0 iki 10 įrašykite tiesiogiai.
+const arry = [];
+
+for(let i = 0; i< 10; i++){
+    let numbers = rand(0, 5)
+    if (numbers === 0) {
+        arry[i] = numbers
+    } else {
+        arry[i] = []
+        for (let x = 0; x < numbers; x++) {
+            numbers = rand(0, 10)
+            arry[i].push(numbers)
+        }
+    }
+}
+
+console.log(arry)
+
+
+// 6uzd. Paskaičiuokite 5 uždavinio masyvo visų reikšmių sumą ir išrūšiuokite masyvą taip, kad pirmiausiai eitų mažiausios masyvo reikšmės arba jeigu reikšmė yra masyvas, to masyvo reikšmių sumos.
+const arrayone = [];
+
+
+for (let i = 0; i < 10; i++) {
+    let numbers = rand(0, 5);
+    if (numbers === 0) {
+        arrayone[i] = numbers;
+    } else {
+        arrayone[i] = [];
+        let sum = 0;
+        for (let x = 0; x < numbers; x++) {
+            let randomNumber = rand(0, 10);
+            arrayone[i].push(randomNumber);
+            sum += randomNumber;
+        }
+        arrayone[i].sum = sum; // Sukuriame savybę sum, kuri saugos šio masyvo reikšmių sumą
+    }
+}
+console.log(arrayone)
+
+let totalSum = 0
+
+
+
+// 7 uzd. issifruokite kas parasyta siame stringe "33!115!109!101!105!115!105!118!32!111!105!108!97!103!116!105!97!118!97!115!32!115!117!97!382!97!114!71!";
+
+const encryptedMessage = "33!115!109!101!105!115!105!118!32!111!105!108!97!103!116!105!97!118!97!115!32!115!117!97!382!97!114!71!";
+const decryptedMessage = encryptedMessage.split("!").map(char => String.fromCharCode(parseInt(char))).join("");
+console.log(decryptedMessage);
---
+++ 20240212/uzd./skaiciai.js
@@ -0,0 +1,66 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+  
+// const masyvas = [];
+// const masyvas2 = [0, 2, 4, 8, 16, 32, 64]
+// const box = document.querySelector('.box');
+// console.log(box);
+
+
+// for( let i = 0; i < 16; i++ ) {    
+  
+//     let number = masyvas2[rand(0, 6)];
+//     // 0, 2, 4, 8, 16, 32, 64
+//     masyvas.push(number);
+
+//     if(masyvas[i] === 0) {
+//         box.innerHTML += `<div style="background-color:#cdc1b4">&nbsp</div>`
+//     } else if (masyvas[i] === 2) {
+//         box.innerHTML += `<div style="background-color:#eee4da; color:#776e65">${masyvas[i]}</div>`
+//     } else if (masyvas[i] === 4) {
+//         box.innerHTML += `<div style="background-color:#eee1c8; color:#776e65">${masyvas[i]}</div>`
+//     } else if (masyvas[i] === 8) {
+//         box.innerHTML += `<div style="background-color:#f2b27a; color:#f9f6f2">${masyvas[i]}</div>`
+//     } else if (masyvas[i] === 16) {
+//         box.innerHTML += `<div style="background-color:#f69664; color:#f9f6f2">${masyvas[i]}</div>`
+//     } else if (masyvas[i] === 32) {
+//         box.innerHTML += `<div style="background-color:#f67f60; color:#f9f6f2">${masyvas[i]}</div>`
+//     } else if (masyvas[i] === 64) {
+//         box.innerHTML += `<div style="background-color:#f75f3b; color:#f9f6f2">${masyvas[i]}</div>`
+//     }
+// }
+
+function handleClick() {
+    const masyvas = [];
+    const masyvas2 = [0, 2, 4, 8, 16, 32, 64];
+    const box = document.querySelector(".box");
+    console.log(box);
+    box.innerHTML = "";
+    for (let i = 0; i < 16; i++) {
+      let number = masyvas2[rand(0, 6)];
+      // 0, 2, 4, 8, 16, 32, 64
+      masyvas.push(number);
+      if (masyvas[i] === 0) {
+        box.innerHTML += `<div style="background-color:#cdc1b4"></div>`;
+      } else if (masyvas[i] === 2) {
+        box.innerHTML += `<div style="background-color:#eee4da; color:#786e66">${masyvas[i]}</div>`;
+      } else if (masyvas[i] === 4) {
+        box.innerHTML += `<div style="background-color:#eee1c8; color:#786e66">${masyvas[i]}</div>`;
+      } else if (masyvas[i] === 8) {
+        box.innerHTML += `<div style="background-color:#f2b27a; color:#f9f6f2">${masyvas[i]}</div>`;
+      } else if (masyvas[i] === 16) {
+        box.innerHTML += `<div style="background-color:#fa936d; color:#f9f6f2">${masyvas[i]}</div>`;
+      } else if (masyvas[i] === 32) {
+        box.innerHTML += `<div style="background-color:#fb7967; color:#f9f6f2">${masyvas[i]}</div>`;
+      } else if (masyvas[i] === 64) {
+        box.innerHTML += `<div style="background-color: #fc5b47; color:#f9f6f2">${masyvas[i]}</div>`;
+      }
+    }
+  }
+
+  document.querySelector("button").addEventListener("click", handleClick);
+  handleClick()
