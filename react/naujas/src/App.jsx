8dc3c4bf74f7e1ec8937f984f60488c4a1fde32c
LordsVoldemorts <petrauskiene.ieva1993@gmail.com>
2024-02-29 09:22:47 +0200

    react pradzia, css, js

--- react/naujas/src/App.jsx
+++ react/naujas/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/components/logo/Logo'
+import Search from './assets/components/search/Search'
+import Navigation from './assets/components/navigation/Navigation'
+import Intro from './assets/components/intro/Intro'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="container">
+          <Logo />
+          <Search />
+          <Navigation />
+        </div>
+       
+      </header>
+      <main>
+         <div className="intro">
+          <Intro />
+        </div>
+        
+      </main>
     </>
   )
 }
---
+++ react/naujas/src/assets/components/search/Search.jsx
@@ -0,0 +1,11 @@
+import Icon from "../icon/Icon";
+const Search = () => {
+    return (
+        <div>
+        <Icon />
+        <input type="Search" placeholder="Search" />
+        </div>
+    )
+}
+
+export default Search;
